2b7c6b4f31c157e495b8c1c2ddca6cb1c23166f0
Bhaskar <hp@DESKTOP-CQG08NB>
2023-03-13 20:12:54 +0530

    Adding Bhaskar Controller, Adding styling,

---
+++ Char_Project/Controllers/BhaskarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Char_Project.Controllers
+{
+    public class BhaskarController : Controller
+    {
+        // GET: Bhaskar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Yogi()
+        {
+            return View();
+        }
+    }
+}
