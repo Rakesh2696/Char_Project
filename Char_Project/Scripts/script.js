b13ea0b495b26013bf40917c234d9af77f8139f4
Bhaskar <hp@DESKTOP-CQG08NB>
2023-03-14 18:43:30 +0530

    Change UI, Change Styling.

--- Char_Project/Scripts/script.js
+++ Char_Project/Scripts/script.js
@@ -1,67 +1,54 @@
-(function() {
-        var container = document.getElementById("container"),
-            inner = document.getElementById("inner");
+const formBtn1 = document.querySelector("#btn-1")
+        const formBtnPrev2 = document.querySelector("#btn-2-prev")
+        const formBtnNext2 = document.querySelector("#btn-2-next")
+        const formBtn3 = document.querySelector("#btn-3")
 
-        var mouse = {
-            _x: 0,
-            _y: 0,
-            x: 0,
-            y: 0,
-            updatePosition: function(event) {
-                var e = event || window.event;
-                this.x = e.clientX - this._x;
-                this.y = (e.clientY - this._y) * -1;
-            },
-            setOrigin: function(e) {
-                this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-                this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-            },
-            show: function() {
-                return "(" + this.x + ", " + this.y + ")";
-            }
-        };
+        // Button listener of form 1
+        formBtn1.addEventListener("click", function(e) {
+            gotoNextForm(formBtn1, formBtnNext2, 1, 2)
+            e.preventDefault()
+        })
 
-        mouse.setOrigin(container);
+        // Next button listener of form 2
+        formBtnNext2.addEventListener("click", function(e) {
+            gotoNextForm(formBtnNext2, formBtn3, 2, 3)
+            e.preventDefault()
+        })
 
-        var counter = 0;
-        var updateRate = 10;
-        var isTimeToUpdate = function() {
-            return counter++ % updateRate === 0;
-        };
+        // Previous button listener of form 2
+        formBtnPrev2.addEventListener("click", function(e) {
+            gotoNextForm(formBtnNext2, formBtn1, 2, 1)
+            e.preventDefault()
+        })
 
+        // Button listener of form 3
+        formBtn3.addEventListener("click", function(e) {
+            document.querySelector(`.step--3`).classList.remove("step-active")
+            document.querySelector(`.step--4`).classList.add("step-active")
+            formBtn3.parentElement.style.display = "none"
+            document.querySelector(".form--message").innerHTML = `
+   <h1 class="form--message-text">Your account is successfully created </h1>
+   `
+            e.preventDefault()
+        })
 
-        var onMouseEnterHandler = function(event) {
-            update(event);
-        };
-
-        var onMouseLeaveHandler = function() {
-            inner.style = "";
-        };
-
-        var onMouseMoveHandler = function(event) {
-            if (isTimeToUpdate()) {
-                update(event);
-            }
-        };
-
-        var update = function(event) {
-            mouse.updatePosition(event);
-            updateTransformStyle(
-                (mouse.y / inner.offsetHeight / 2).toFixed(2),
-                (mouse.x / inner.offsetWidth / 2).toFixed(2)
-            );
-        };
-
-        var updateTransformStyle = function(x, y) {
-            var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-            inner.style.transform = style;
-            inner.style.webkitTransform = style;
-            inner.style.mozTransform = style;
-            inner.style.msTransform = style;
-            inner.style.oTransform = style;
-        };
-
-        container.onmouseenter = onMouseEnterHandler;
-        container.onmouseleave = onMouseLeaveHandler;
-        container.onmousemove = onMouseMoveHandler;
-    })();
+        const gotoNextForm = (prev, next, stepPrev, stepNext) => {
+            // Get form through the button
+            const prevForm = prev.parentElement
+            const nextForm = next.parentElement
+            const nextStep = document.querySelector(`.step--${stepNext}`)
+            const prevStep = document.querySelector(`.step--${stepPrev}`)
+                // Add active/inactive classes to both previous and next form
+            nextForm.classList.add("form-active")
+            nextForm.classList.add("form-active-animate")
+            prevForm.classList.add("form-inactive")
+                // Change the active step element
+            prevStep.classList.remove("step-active")
+            nextStep.classList.add("step-active")
+                // Remove active/inactive classes to both previous an next form
+            setTimeout(() => {
+                prevForm.classList.remove("form-active")
+                prevForm.classList.remove("form-inactive")
+                nextForm.classList.remove("form-active-animate")
+            }, 1000)
+        }
